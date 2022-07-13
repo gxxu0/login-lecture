deaa154ead5f273c323fe6b7f97b24c48603d4c7
gxxu0 <94434730+gxxu0@users.noreply.github.com>
2022-07-14 01:18:55 +0900

    dom을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,21 @@
 "use strict";
-console.log('hello');
-console.log('bye');
+// console.log('hello');
+// console.log('bye');
+
+//DOM // document object model(인터페이스) --> html에 데이터를 가져와서 제어
+const id = document.querySelector("#id"), 
+    psword = document.querySelector("#psword"), 
+    loginBtn = document.querySelector("button"); //id --> login.ejs에 있는 태그들의 정보
+//console.log(id);
+loginBtn.addEventListener("click", login);
+
+function login(){ //버튼 클릭 시 login 정보 가져오기 가능
+    //console.log(id.value);
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    
+    console.log(req);
+    //fetch();
+}
